3ba442ceee4e29855a8092638fe112262c871570
DHughes666 <graziemolto6@gmail.com>
2023-08-29 04:32:13 +0100

    User authentication initiated - update

--- reactfrontend/src/auth/helper/index.js
+++ reactfrontend/src/auth/helper/index.js
@@ -16,3 +16,60 @@ export const signup = async (user) => {
         return console.log(err);
     }
 }
+
+
+export const sigin = async user => {
+    const formData = new FormData()
+
+    for (const name in user) {
+        formData.append(name, user[name])
+    }
+
+    try {
+        const response = await fetch(`${API}/user/login/`, {
+            method: 'POST',
+            body: formData
+        });
+        return response.json;
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+
+export const authenticate = (data, next) => {
+    if (typeof window !== undefined) {
+        localStorage.setItem("customCookie", JSON.stringify(data));
+        next();
+    }
+};
+
+export const isAuthenticated = () => {
+    if (typeof window == undefined) {
+        return false;
+    }
+    if (localStorage.getItem("customCookie")) {
+        return JSON.parse(localStorage.getItem("customCookie"));
+    } else {
+        return false;
+    }
+}
+
+export const signout = (next) => {
+    const userId = isAuthenticated() && isAuthenticated().user.id
+    
+    if (typeof window !== undefined) {
+        localStorage.removeItem("customCookie");
+        cartEmpty(() => {})
+        // next();
+
+        return fetch(`${API}/user/logout/${userId}`, {
+            method: 'GET',
+        })
+        .then(response => {
+            console.log("Signout success")
+            next();
+        })
+        .catch(err => console.log(err));
+    }
+}
